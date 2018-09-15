1134de4aa660681de432d0f587b6b3f7ac687775
Miste <misteboss1@gmail.com>
2018-09-15 14:44:29 +0200

    The bot is now posting when a new version of the BedrockServer is available.

---
+++ src/tasks/CheckBedrockServerTask.js
@@ -0,0 +1,71 @@
+require('./../BedrockUpdateBot.js')
+var request = require('request');
+const Discord = require('discord.js');
+
+class CheckBedrockServerTask {
+    static getDelay() {
+        return 30000;
+    }
+
+    static getName() {
+        return "CheckBedrockServerTask";
+    }
+
+    static check(Bot) {
+        var url = "http://145.239.168.187/BedrockServer.php"
+        request({
+            url: url,
+            json: true
+        }, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                if (body.length === 2) {
+                    if (body[0] !== botManager.config['BSWin10']) {
+                        var win10Version = body[0].split("bedrock-server-")[1].split(".zip")[0];
+
+                        botManager.config["BSWin10"] = body[0];
+                        botManager.saveConfig()
+
+                        var embed = new Discord.RichEmbed()
+                            .setTitle('A new version of the BedrockServer Win10 is available for: ' + win10Version + " :pushpin:")
+                            .setColor('#0941a9')
+                            .setAuthor("BedrockUpdateBot", botManager.avatarURL)
+                        botManager.sendToChannels('news', embed)
+                        botManager.Bot.users.forEach(function (element) {
+                            if (element.id == botManager.config['ownerId']) {
+                                element.send(embed);
+                            }
+                        });
+                        botManager.client.post('statuses/update', { status: '📌 A new version of the BedrockServer Win10 is available for: ' + win10Version + " !\n\n#RT" }, function (error, tweet, response) { });
+                    }
+                    if (body[1] !== botManager.config['BSLinux']) {
+                        var linuxVersion = body[1].split("bedrock-server-")[1].split(".zip")[0];
+
+                        botManager.config["BSLinux"] = body[0];
+                        botManager.saveConfig()
+
+                        var embed = new Discord.RichEmbed()
+                            .setTitle('A new version of the BedrockServer Linux is available for: ' + linuxVersion + " :pushpin:")
+                            .setColor('#0941a9')
+                            .setAuthor("BedrockUpdateBot", botManager.avatarURL)
+                        botManager.sendToChannels('news', embed)
+                        botManager.Bot.users.forEach(function (element) {
+                            if (element.id == botManager.config['ownerId']) {
+                                element.send(embed);
+                            }
+                        });
+                        botManager.client.post('statuses/update', { status: '📌 A new version of the BedrockServer Linux is available for: ' + linuxVersion + " !\n\n#RT" }, function (error, tweet, response) { });
+                    }
+
+                } else {
+                    Bot.users.forEach(function (element) {
+                        if (element.id == botManager.config['ownerId']) {
+                            element.send("Incorrect body length for BedrockServer array !");
+                        }
+                    });
+                }
+            }
+        })
+    }
+}
+
+module.exports = CheckBedrockServerTask;
